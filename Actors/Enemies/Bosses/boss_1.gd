06307eb3061a1c54d1bd2d5a24fa4a48c92fc713
nathankeyt <nake8894@colorado.edu>
2024-09-11 18:31:15 -0600

    projectile + ui additions

--- Actors/Enemies/Bosses/boss_1.gd
+++ Actors/Enemies/Bosses/boss_1.gd
@@ -6,8 +6,8 @@ const SPEED = 300.0
 
 
 func _physics_process(delta: float) -> void:
-	velocity.x = 10.0
+	#velocity.x = 10.0
 	
-	animation_tree.set("parameters/WalkCycle/blend_position", velocity.length())
+	#animation_tree.set("parameters/WalkCycle/blend_position", velocity.length())
 
 	move_and_slide()
--- Actors/Player/player.gd
+++ Actors/Player/player.gd
@@ -1,18 +1,18 @@
 extends CharacterBody2D
 
 
-@export var speed: float = 300.0
+@export var speed: float = 150.0
 @export var animation_tree: AnimationTree
+@export var default_projectile_scene: Resource
+@export var spirit_projectile_scene: Resource
 
 var last_direction: Vector2 = Vector2.DOWN
+var spirit_power: int = 3;
 
 
 func _physics_process(delta: float) -> void:
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	
-	var direction := Input.get_vector("left", "right", "up", "down")
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	
 	if direction:
 		velocity = direction * speed
@@ -23,7 +23,26 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y, 0, speed)
 		velocity.x = move_toward(velocity.x, 0, speed)
 		
-	
-		
-
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fire"):
+		fire_projectile(default_projectile_scene)
+	if event.is_action_pressed("alt_fire"):
+		fire_spirit_projectile(spirit_projectile_scene)
+
+func fire_projectile(projectile_scene: Resource):
+	var new_projectile: Area2D = projectile_scene.instantiate()
+	get_parent().add_child(new_projectile)
+	
+	print(get_viewport().get_mouse_position())
+		
+	var projectile_forward: Vector2 = (get_global_mouse_position() - position).normalized()
+	new_projectile.fire(projectile_forward, 1000.0)
+	new_projectile.position = position + (projectile_forward * 20.0);
+
+func fire_spirit_projectile(projectile_scene: Resource):
+	spirit_power -= 1;
+	fire_projectile(projectile_scene);
+	
+	
